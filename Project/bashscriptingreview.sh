82e5ab2c0608b7aacf076b8a9309c2e70e310920
SjoekenVM <beauroels@gmail.com>
2022-01-05 15:24:04 +0100

    Testing bash script review

---
+++ Project/bashscriptingreview.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+IP_HOST=192.168.56.107
+ping -c 5 $IP_HOST 
+INTERFACE=GigabitEthernet1 #Your ethernet interface
+USERNAME=cisco
+PASSWORD=cisco123! #standard cisco password
+status_code=$(curl -ks \
+-w "%{http_code}" \
+-o /dev/null \
+-u "$USERNAME:$PASSWORD" \
+-H "Accept: application/yang-data+json" \
+"https://$IP_HOST/restconf/data/ietf-interfaces:interfaces/interface=$INTERFACE")
+
+echo $status_code
+
+if [ $status_code -eq 200 ]
+then 
+   echo "Yes - interface is up - returning status code: 200"
+else
+   echo "No - interface is down - returning status code: $status_code"
+fi
