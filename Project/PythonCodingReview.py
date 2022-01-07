cebe6f2c7905321114a0558f3e239844d5df7512
SjoekenVM <beauroels@gmail.com>
2022-01-07 15:41:40 +0100

    Python review update

--- Project/PythonCodingReview.py
+++ Project/PythonCodingReview.py
@@ -1,8 +1,8 @@
 import requests
 import json
-current_access_token = "valid token"
+current_access_token = "ZDE4ZDg2NmQtMDUxYi00OGE3LWIzYTgtMmRkMWE4YWRkNmYxOWE0ZTVlMTYtNDI3_P0A1_5998ff09-9c27-4407-818f-2305709e8d49"
 uri_scheme = 'https://'
-uri_authority_server = 'api.ciscopark.com'
+uri_authority_server = 'webexapis.com'
 uri_api_path = '/v1/people/me'
 url = uri_scheme + uri_authority_server + uri_api_path
 
