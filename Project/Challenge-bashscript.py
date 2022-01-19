ee6647b71f146317425e831e9fac7a201fce6d85
SjoekenVM <beauroels@gmail.com>
2022-01-19 09:29:53 +0100

    challenge bash script added

---
+++ Project/Challenge-bashscript.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+iphost = "192.168.0.104"
+interface = "GigabitEthernet1"
+username = "ikwildood"
+
+
+#Curl in python
+
+headers = {
+    'Accept': 'application/yang-data+json',
+}
+
+response = requests.get('https://'+ iphost+ '/restconf/data/ietf-interfaces:interfaces/interface=' + interface, headers=headers, verify=False, auth=('$USERNAME', '$PASSWORD'))
+
+
+
+if response.status_code==200:
+    print("status ok")
+   
+else:
+    print("status not ok")
